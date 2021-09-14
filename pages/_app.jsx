753f801dd23df029b599de87c694c7c4c41d4dc6
Justin Morgan <justin.morgan@hey.com>
2021-09-14 15:47:53 -0700

    :wrench:  Change vh fix to alternative

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,16 +1,43 @@
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools'
+import Script from "next/script";
 import "./global.css";
 
 if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
   require("../mocks");
 }
 
+const postcss_vh_polyfill = `
+var customViewportCorrectionVariable = 'vh';
+
+
+function setViewportProperty(doc) {
+  var prevClientHeight;
+  var customVar = '--' + ( customViewportCorrectionVariable || 'vh' );
+  function handleResize() {
+    var clientHeight = doc.clientHeight;
+    if (clientHeight === prevClientHeight) return;
+    requestAnimationFrame(function updateViewportHeight(){
+      doc.style.setProperty(customVar, (clientHeight * 0.01) + 'px');
+      prevClientHeight = clientHeight;
+    });
+  }
+  handleResize();
+  return handleResize;
+}
+window.addEventListener('resize', setViewportProperty(document.documentElement));
+`;
+
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }) {
   return (
     <QueryClientProvider client={queryClient}>
+      <Script
+        id="postcss_vh_polyfill"
+        dangerouslySetInnerHTML={{ __html: postcss_vh_polyfill }}
+      />
+
       <Component {...pageProps} />
       {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
